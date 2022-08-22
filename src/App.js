ff15361e1c4af4a728b8086a3adf5de4cedeffa4
hye-ri <kwonhr01@naver.com>
2022-08-22 21:18:24 +0900

    무비 어플리케이션, async await 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+    /* 
+    1. application reder
+    2. lading이 true니까 로딩 보여줌
+    3. application이 mount된 후 우리는 getMovies function 호출 
+    4.getMovies는 axios.get을 사용하고 axios.get은 완료 되기까지 시간이 조금 필요해서 await를 넣어준다. 
+    5. async await를 하는 것은 우리가 js에게 getMovies function이 시간이 좀 필요하고 우리는 그걸 기다려야 한다는 것을 말하는 것
+    
+    */
     state = {
-        count: 0,
+        isLoading: true,
+        movies: [],
     };
-    add = () => {
-        /* this.setState({ count: this.state.count + 1 }); */
-        this.setState((current) => ({ count: current.count + 1 }));
+    getMovies = async () => {
+        const movies = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json"
+        ); /* data를 잡기 위해 const movies라고 지정 axious는 느리기 때문에 js에게 componentDidMount가 끝날 때 까지 약간 시간이 걸릴 수 있다고 말해야 된다. 그래서 새로운 함수를 생성하고 async = await으로 만들어주고 그걸 호출 해줘도 된다. => 이 함수가 비동기라고 말한 것. 너는 이걸 기다려야해 라는 뜻이다.  */
     };
-    minus = () => {
-        //this.setState({ count: this.state.count - 1 });
-        this.setState((current) => ({ count: current.count - 1 }));
-    };
+    async componentDidMount() {
+        /*   setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, 6000); */
+        this.getMovies();
+    }
     render() {
-        return (
-            <div>
-                <h1>The number is : {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        );
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading" : "We are ready"}</div>;
     }
 }
 
---
+++ src/Test.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("hello");
+    }
+    state = {
+        count: 0,
+    };
+    add = () => {
+        /* this.setState({ count: this.state.count + 1 }); */
+        this.setState((current) => ({ count: current.count + 1 }));
+    };
+    minus = () => {
+        //this.setState({ count: this.state.count - 1 });
+        this.setState((current) => ({ count: current.count - 1 }));
+    };
+    componentDidMount() {
+        console.log("component rendered");
+    }
+    componentDidUpdate() {
+        console.log("i just update!");
+    }
+    componentWillUnmount() {
+        console.log("good bye");
+    }
+    render() {
+        console.log("i am rendering");
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
+}
+
+export default App;
